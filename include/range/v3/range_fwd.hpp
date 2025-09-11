b53c4eb5b4fb7120dca31ecea72d25f2e5fcdd00
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-09-11 16:42:28 +0200

    Add boorowed ranges for `range-v3`

--- include/range/v3/range_fwd.hpp
+++ include/range/v3/range_fwd.hpp
@@ -944,6 +944,11 @@ namespace ranges
 /// \endcond
 ///
 
+#ifdef RANGE_V3_COMBINE_WITH_STD
+CPP_template(class T)(requires ::ranges::enable_borrowed_range<T>)
+inline constexpr bool std::ranges::enable_borrowed_range = true;
+#endif
+
 RANGES_DIAGNOSTIC_POP
 
 #include <range/v3/detail/epilogue.hpp>
