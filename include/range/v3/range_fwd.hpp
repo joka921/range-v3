e84be9d671fbf91e2d977aaaf17a9dd34aaa127d
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-11-26 16:58:52 +0100

    Fix a merge collision.

--- include/range/v3/range_fwd.hpp
+++ include/range/v3/range_fwd.hpp
@@ -946,11 +946,6 @@ namespace ranges
 
 #ifdef RANGE_V3_COMBINE_WITH_STD
 CPP_template(class T)(requires ::ranges::enable_borrowed_range<T>)
-inline constexpr bool std::ranges::enable_borrowed_range = true;
-#endif
-
-#ifdef RANGE_V3_COMBINE_WITH_STD
-CPP_template(class T)(requires ::ranges::enable_borrowed_range<T>)
 inline constexpr bool std::ranges::enable_borrowed_range<T> = true;
 #endif
 
